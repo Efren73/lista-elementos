334af4bb6bf1cebacfe5b69bf66eda7611cd7e0d
Efren73 <A01276266@itesm.mx>
2021-02-28 23:50:39 -0600

    Actividad tamales

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
 function App() {
+  let result = {
+    status: 200,
+    statusText: "Success",
+    data: [
+      { details: "dulce", name: "Tamal de dulce", price: "$12.00" },
+      { details: "picante", name: "Tamal verde", price: "$10.00" },
+      { details: "picante", name: "Tamal rojo", price: "$10.50" },
+      { details: "picante", name: "Tamal de rajas", price: "$11.00" }
+    ]
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <h1>Tamalitos</h1>
+      <div class="tamalitos">
+        <ul class="tamales">
+            <li class="encabezado">{result.data[0].name}</li>
+            <li class="precio">{result.data[0].price}</li>
+            <li class="sabordulce" >{result.data[0].details}</li>
+        </ul>
+        <ul class="tamales">
+            <li class="encabezado">{result.data[1].name}</li>
+            <li class="precio">{result.data[1].price}</li>
+            <li class="saborpicante" >{result.data[1].details}</li>
+        </ul>
+        <ul class="tamales">
+            <li class="encabezado">{result.data[2].name}</li>
+            <li class="precio">{result.data[2].price}</li>
+            <li class="saborpicante" >{result.data[2].details}</li>
+        </ul>
+        <ul class="tamales">
+            <li class="encabezado">{result.data[3].name}</li>
+            <li class="precio">{result.data[3].price}</li>
+            <li class="saborpicante" >{result.data[3].details}</li>
+        </ul>
+      </div>
+    </body>
   );
 }
 
